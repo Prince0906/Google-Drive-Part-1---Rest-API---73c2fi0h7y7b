2db8ce6d3a10c98c3063522f17f5e6a90be62e95
Prince0906 <princesahoo659@gmail.com>
2024-09-05 15:31:16 +0530

    changed creatAfile

--- tools/createAfile.js
+++ tools/createAfile.js
@@ -6,7 +6,13 @@ import { fileURLToPath } from "url";
 const createAfile = (fileName, fileData) => {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const p = path.join(__dirname, "..", "root", fileName);
-    return true;
+    try {
+        fs.writeFileSync(p, fileData);
+        return true;
+    }
+    catch(e) {
+        return False;
+    }
 
 };
 
